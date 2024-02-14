8ff31db34977c0c31228e6ed89afa3b70f69e410
centopw <tanhiep26@gmail.com>
2024-02-14 17:27:03 +0100

    feat: remove Jumbotron

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,13 +1,11 @@
 import FollowCard from '@/components/FollowCard'
 import Hero from '@/components/Hero'
-import Jumbotron from '@/components/Jumbotron'
 import LastestPost from '@/components/LastestPost'
 
 export default function Home({ posts }) {
   return (
     <>
       <Hero />
-      <Jumbotron />
       <LastestPost posts={posts} />
       <FollowCard />
     </>
