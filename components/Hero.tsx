177b7de860bafa39c50d066148fc1ddb4bba3240
Hiep Nguyen <30675552+centopw@users.noreply.github.com>
2024-02-27 19:11:48 +0100

    style: fix CSS class name and remove unnecessary XML declaration (#15)
    * style: fix CSS class name and remove unnecessary XML declaration
    
    The CSS class name "o overflow-hidden" in the HTML file was changed to "overflow-hidden" to fix a styling issue. Additionally, the unnecessary XML declaration was removed from the SVG file.
    
    * feat: preview new theme

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
 
   return (
     <>
-      <div className="o overflow-hidden">
+      <div className="overflow-hidden">
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
