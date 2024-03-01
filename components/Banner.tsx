7ad81335ff69f27694f21ae2892f23522d040758
Hiep Nguyen <30675552+centopw@users.noreply.github.com>
2024-03-01 14:40:03 +0100

    feat: add Banner component (#17)

---
+++ components/Banner.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className="bg-orange-400 py-11 text-center text-6xl font-bold text-white">
+      <p>
+        Create with <span className="bg-white px-3 py-2 text-orange-400">purpose</span>, design with{' '}
+        <span className="underline">passion</span>
+      </p>
+    </div>
+  )
+}
+
+export default Banner
